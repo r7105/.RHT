b6c32b470faf7d24601fb5dfb3d3aa04f2234c63
SMg <139099690+speedevil50@users.noreply.github.com>
2025-03-10 14:04:46 -0400

    Create interpreter.py

---
+++ HumanaT/src/interpreter.py
@@ -0,0 +1,45 @@
+class Interpreter:
+    def __init__(self, statements):
+        self.statements = statements
+        self.variables = {}
+
+    def run(self):
+        for statement in self.statements:
+            self.execute(statement)
+
+    def execute(self, statement):
+        if statement[0] == 'printStmt':
+            print(statement[1][1:-1])
+        elif statement[0] == 'assignStmt':
+            self.variables[statement[1]] = self.evaluate(statement[2])
+        elif statement[0] == 'ifStmt':
+            if self.evaluate(statement[1]):
+                self.execute(statement[2])
+        elif statement[0] == 'loopStmt':
+            for _ in range(statement[1]):
+                self.execute(statement[2])
+        elif statement[0] == 'whileStmt':
+            while self.evaluate(statement[1]):
+                for stmt in statement[2]:
+                    self.execute(stmt)
+        else:
+            raise RuntimeError('Unknown statement: ' + statement[0])
+
+    def evaluate(self, expression):
+        if expression[0] == 'number':
+            return int(expression[1])
+        elif expression[0] == 'string':
+            return expression[1][1:-1]
+        elif expression[0] == 'identifier':
+            return self.variables[expression[1]]
+        elif expression[0] in ('is greater than', 'is less than', 'is equal to'):
+            left = self.evaluate(expression[1])
+            right = self.evaluate(expression[2])
+            if expression[0] == 'is greater than':
+                return left > right
+            elif expression[0] == 'is less than':
+                return left < right
+            elif expression[0] == 'is equal to':
+                return left == right
+        else:
+            raise RuntimeError('Unknown expression: ' + expression[0])
